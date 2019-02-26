aaea19748dd2ad8845bdbdf51b75bcd71b80aaf4
nammapdientrai <nammapdientrai@gmail.com>
2019-02-26 13:08:19 +0700

    update project

---
+++ tests/model/productTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+include 'controller/productController.php';
+
+class StupidTest extends TestCase
+{
+    public function testTrueIsTrue(){
+    	$foo = true;
+    	$this->assertTrue($foo);
+    }
+
+    public function testTotal() {
+    	$a = 2 * 5;
+	$this->assertTrue( ($a == 10) );
+    }
+
+    public function testMysql() {
+     	$db = new ProductController();
+	$list = $db->getAllProduct();
+
+	$this->assertTrue( ( $list[0]->get_id() == '1' ) );
+    }
+}
